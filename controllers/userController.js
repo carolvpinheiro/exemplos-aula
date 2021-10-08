e390813a7d490aae7cd739fcd8deeb30d1274dd8
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-10-08 12:07:25 -0300

    Configurando controllers e rota user

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { User } = require('../models');
+const router = express.Router();
+
+router.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+// ...
+
+module.exports = router;
